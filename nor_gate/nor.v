678b1c63e973319e47d54f56ce1455d504deda03
Muhammad Wamiq <muhammadwamiqkhan003@gmail.com>
2024-05-26 19:03:25 +0500

    initial commit

---
+++ nor_gate/nor.v
@@ -0,0 +1,29 @@
+module top (
+    input wire btn1, 
+    input wire btn2,
+    output reg [5:0] out_led
+);
+
+initial begin
+    out_led[0] = 1;
+    out_led[1] = 1;
+    out_led[2] = 1;
+    out_led[3] = 1;
+    out_led[4] = 1;
+    out_led[5] = 1;
+
+end
+
+always @(*) 
+begin
+    if (~btn1 && ~btn2) 
+        out_led[1] = 0;
+    else if (~btn1 && btn2) 
+        out_led[1] = 1;
+    else if (btn1 && ~btn2) 
+        out_led[1] = 1;
+    else 
+      out_led[1] = 1;
+
+end
+endmodule
